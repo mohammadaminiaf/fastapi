781dd20abf103ff98ebf6b24375da6ac742e28d1
Mohammad Amini <132151000+mohammadaminiaf@users.noreply.github.com>
2025-02-16 05:13:59 +0430

    Added a new route

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/database")
+async def get_database_url():
+    db_url = os.getenv('DATABASE_URL')
+    print('******************************************************')
+    print(f'DATABASE_URL: {db_url}')
+    print('******************************************************')
